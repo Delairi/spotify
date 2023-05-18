23ace0bc3d6ba09b9c77cb714d18f773e4fbd382
adriansw20@hotmail.com <adriansw20@hotmail.com>
2023-05-17 22:24:20 -0500

    files

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 const main = async () => {
 
     client.on('connect', () => { console.log('Redis client connected') })
-    await client.connect().catch(err => console.log(err, '??'))
+    await client.connect().catch(err => console.log(err))
     await ModelTokenSpotify().then(() => console.log('Model Token Spotify created')).catch(err => console.log(err));
     await ModelSpotifyQuery().then(() => console.log('Model Spotify Query created')).catch(err => console.log(err));
     const app = express()
@@ -29,6 +29,12 @@ const main = async () => {
 
 
     app.use('/api/v1', UserToken, TokenActive, routerGeneral)
+
+    app.get('/*', (req, res) => {
+
+        res.sendFile(__dirname + './dist/index.html')
+
+    })
 
     app.listen(4004, () => {
 
